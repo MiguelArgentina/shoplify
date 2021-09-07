11a89ee06c97dacb42a9cac60c2aa3753ba44549
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-07 17:17:44 -0300

    add stripe_product_id and price to local products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,17 @@ class Product < ApplicationRecord
     name
   end
   
+  after_create do
+      product = Stripe::Product.create({
+      name: name,
+    })
+
+    price = Stripe::Price.create({
+      product: product.id,
+      unit_amount: self.price,
+      currency: 'usd',
+    })
+
+    update(stripe_product_id: product.id)
+  end
 end
