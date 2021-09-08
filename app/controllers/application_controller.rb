b1d8417d6fad3e30da2e28e98c314a7041f034b2
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-08 16:16:30 -0300

    Add Shopping Cart to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :initialize_session
+  before_action :load_cart
+
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root to: "products#index"
+  root to: 'products#index'
   resources :products
   resources :checkout, only: [:create]
   resources :webhooks, only: [:create]
-  get "success", to: "checkout#success"
-  get "failure", to: "checkout#failure"
+  get 'success', to: 'checkout#success'
+  get 'failure', to: 'checkout#failure'
+  post 'products/add_to_cart/:id', to: 'products#add_to_cart',
+                                   as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart',
+                                          as: 'remove_from_cart'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
