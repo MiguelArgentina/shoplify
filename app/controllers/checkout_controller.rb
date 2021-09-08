18c6db801a02688b9d7d510c1a2d40bd0611a77c
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-08 01:20:49 -0300

    Create controllers and views for success and failure in stripe

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -12,11 +12,36 @@ class CheckoutController < ApplicationController
           'card',
         ],
         mode: 'payment',
-        success_url: root_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: root_url,
+        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: failure_url,
       })
    respond_to do |format|
     format.js
    end
+  end
+
+  def success
+    session_id = params[:session_id]
+    @session = get_session_id(session_id)
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    @session_with_expand = Stripe::Checkout::Session.retrieve({
+                                                                   id: session_id,
+                                                                   expand: %w[payment_intent line_items]
+                                                                 })
+  end
+
+  def failure
+
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def get_session_id(session_id)
+    Stripe::Checkout::Session.retrieve(session_id)
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.permit(:session_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :products
   resources :checkout, only: [:create]
   resources :webhooks, only: [:create]
+  get "success", to: "checkout#success"
+  get "failure", to: "checkout#failure"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
