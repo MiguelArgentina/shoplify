408c77ccd2745a7944cbda95a601fa9d7ed2bcec
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-09-08 01:40:45 -0300

    move logic from view to controller after checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,36 +2,37 @@ class CheckoutController < ApplicationController
 
   def create
     product = Product.find(params[:id])
-      @session = Stripe::Checkout::Session.create({
-        customer: current_user.stripe_customer_id,
-        line_items: [{
-          price: product.stripe_price_id,
-          quantity: 1,
-        }],
-        payment_method_types: [
-          'card',
-        ],
-        mode: 'payment',
-        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: failure_url,
-      })
-   respond_to do |format|
-    format.js
-   end
+    @session = Stripe::Checkout::Session.create({
+                                                  customer: current_user.stripe_customer_id,
+                                                  line_items: [{
+                                                    price: product.stripe_price_id,
+        quantity: 1,
+                                                  }],
+                                                  payment_method_types: [
+                                                    'card',
+                                                  ],
+                                                  mode: 'payment',
+                                                  success_url: success_url + '?session_id={CHECKOUT_SESSION_ID}',
+                                                  cancel_url: failure_url,
+                                                })
+    respond_to do |format|
+      format.js
+    end
   end
 
   def success
     session_id = params[:session_id]
-    @session = get_session_id(session_id)
-    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    @session_with_expand = Stripe::Checkout::Session.retrieve({
-                                                                   id: session_id,
-                                                                   expand: %w[payment_intent line_items]
-                                                                 })
+    session = get_session_id(session_id)
+    session_with_expand =
+      Stripe::Checkout::Session.retrieve({ id: session_id,
+                                           expand: %w[payment_intent line_items] })
+    session_with_expand.line_items.data.each do |line_item|
+      @product = Product.find_by(stripe_product_id: line_item.price.product)
+      @product.increment!(:sales_count)
+    end
   end
 
   def failure
-
   end
 
   private
