f6e0acd2b370b0c1383b0dbe62871258537b5ea1
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2022-01-09 20:52:24 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 
 # PRODUCT
 Product.destroy_all
-product1 = Product.create({:name=>"tomato", :price => 1})
-product2 = Product.create({:name=>"milk", :price => 3})
-product3 = Product.create({:name=>"bread", :price => 5.50})
-product4 = Product.create({:name=>"bacon", :price => 10})
-product5 = Product.create({:name=>"cheese", :price => 3.20})
+product1 = Product.create({:name=>"tomato", :price => 100})
+product2 = Product.create({:name=>"milk", :price => 300})
+product3 = Product.create({:name=>"bread", :price => 550})
+product4 = Product.create({:name=>"bacon", :price => 100})
+product5 = Product.create({:name=>"cheese", :price => 320})
 
 puts "Total number of products: #{Product.all.count}"
 puts "Product names: #{Product.all.pluck("name")}"
