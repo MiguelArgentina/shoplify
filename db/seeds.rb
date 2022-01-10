d1af8278e919f9353aee14dc14e2ff0ad8d8a5f6
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2022-01-09 21:08:27 -0300

    update config for assets in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.destroy_all
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development? && AdminUser.find_by(id:'admin@example.com').present?
 
 # PRODUCT
